4f57acfb423ca7c21ee5771f6f5d75206d78c226
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2013-08-28 22:46:00 +0900

    Initial Commit

---
+++ PTRC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PTRC
+//
+//  Created by Hiroyuki Onishi on 8/28/13.
+//  Copyright (c) 2013 Hiroyuki Onishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
